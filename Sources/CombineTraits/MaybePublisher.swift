ed1585cda765d5a07167b31b5a45110b05045175
Gwendal Roué <gr@pierlis.com>
2020-10-01 12:02:11 +0200

    MaybeResult convenience methods

--- Sources/CombineTraits/MaybePublisher.swift
+++ Sources/CombineTraits/MaybePublisher.swift
@@ -127,6 +127,119 @@ public enum MaybeResult<Success, Failure: Error> {
 }
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension MaybeResult {
+    /// Returns a new `MaybeResult`, mapping any success value using the given
+    /// transformation.
+    ///
+    /// - Parameter transform: A closure that takes the success value of this
+    ///   instance.
+    /// - Returns: A `MaybeResult` instance with the result of evaluating
+    ///   `transform` as the new success value if this instance represents
+    ///   a success.
+    @inlinable
+    public func map<NewSuccess>(_ transform: (Success) -> NewSuccess)
+    -> MaybeResult<NewSuccess, Failure>
+    {
+        switch self {
+        case .empty:
+            return .empty
+        case let .success(success):
+            return .success(transform(success))
+        case let .failure(failure):
+            return .failure(failure)
+        }
+    }
+    
+    /// Returns a new `MaybeResult`, mapping any failure value using the given
+    /// transformation.
+    ///
+    /// - Parameter transform: A closure that takes the failure value of the
+    ///   instance.
+    /// - Returns: A `MaybeResult` instance with the result of evaluating
+    ///   `transform` as the new failure value if this instance represents
+    ///   a failure.
+    @inlinable
+    public func mapError<NewFailure>(_ transform: (Failure) -> NewFailure)
+    -> MaybeResult<Success, NewFailure>
+    {
+        switch self {
+        case .empty:
+            return .empty
+        case let .success(success):
+            return .success(success)
+        case let .failure(failure):
+            return .failure(transform(failure))
+        }
+    }
+    
+    /// Returns a new `MaybeResult`, mapping any success value using the given
+    /// transformation and unwrapping the produced result.
+    ///
+    /// - Parameter transform: A closure that takes the success value of the
+    ///   instance.
+    /// - Returns: A `MaybeResult` instance with the result of evaluating
+    ///   `transform` as the new failure value if this instance represents
+    ///   a success.
+    @inlinable
+    public func flatMap<NewSuccess>(_ transform: (Success) -> MaybeResult<NewSuccess, Failure>)
+    -> MaybeResult<NewSuccess, Failure>
+    {
+        switch self {
+        case .empty:
+            return .empty
+        case let .success(success):
+            return transform(success)
+        case let .failure(failure):
+            return .failure(failure)
+        }
+    }
+    
+    /// Returns a new `MaybeResult`, mapping any failure value using the given
+    /// transformation and unwrapping the produced result.
+    ///
+    /// - Parameter transform: A closure that takes the failure value of the
+    ///   instance.
+    /// - Returns: A `MaybeResult` instance, either from the closure or the
+    ///   previous `.success`.
+    @inlinable
+    public func flatMapError<NewFailure>(
+        _ transform: (Failure) -> MaybeResult<Success, NewFailure>
+    ) -> MaybeResult<Success, NewFailure> {
+        switch self {
+        case .empty:
+            return .empty
+        case let .success(success):
+            return .success(success)
+        case let .failure(failure):
+            return transform(failure)
+        }
+    }
+    
+    /// Returns the success value, if any, as a throwing expression.
+    ///
+    /// - Returns: The success value, if the instance represents a success, or
+    ///   nil if the instance is empty.
+    /// - Throws: The failure value, if the instance represents a failure.
+    @inlinable
+    public func get() throws -> Success? {
+        switch self {
+        case .empty:
+            return nil
+        case let .success(success):
+            return success
+        case let .failure(failure):
+            throw failure
+        }
+    }
+}
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension MaybeResult: Equatable where Success: Equatable, Failure: Equatable { }
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension MaybeResult: Hashable where Success: Hashable, Failure: Hashable { }
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension MaybePublisher {
     /// Wraps this maybe publisher with a type eraser.
     ///
