e400da1344dba3860e54760fec88c4e988007003
Gwendal Roué <gr@pierlis.com>
2020-10-11 14:34:21 +0200

    Documentation

--- Sources/CombineTraits/TraitPublishers/PublisherTraitsMaybe.swift
+++ Sources/CombineTraits/TraitPublishers/PublisherTraitsMaybe.swift
@@ -1,13 +1,49 @@
 import Combine
 
 extension TraitPublishers {
-    // TODO: doc
+    /// `TraitPublishers.Maybe` is a ready-made Combine [Publisher] which allows
+    /// you to dynamically send success or failure events.
+    ///
+    /// It lets you easily create custom maybe publishers to wrap most
+    /// non-publisher asynchronous work.
+    ///
+    /// You create this publisher by providing a closure. This closure runs when
+    /// the publisher is subscribed to. It returns a cancellable object in which
+    /// you define any cleanup actions to execute when the publisher completes,
+    /// or when the subscription is canceled.
+    ///
+    ///     let publisher = TraitPublishers.Maybe<String, MyError> { promise in
+    ///         // Eventually send completion event, now or in the future:
+    ///         promise(.empty)
+    ///         // OR
+    ///         promise(.success("Alice"))
+    ///         // OR
+    ///         promise(.failure(MyError()))
+    ///
+    ///         return AnyCancellable {
+    ///             // Perform cleanup
+    ///         }
+    ///     }
+    ///
+    /// `TraitPublishers.Maybe` is a "deferred" maybe publisher:
+    ///
+    /// - Nothing happens until the publisher is subscribed to. A new job starts
+    ///   on each subscription.
+    /// - It can complete right on subscription, or at any time in the future.
+    ///
+    /// When needed, `TraitPublishers.Maybe` can forward its job to another
+    /// maybe publisher:
+    ///
+    ///     let publisher = TraitPublishers.Maybe<String, MyError> { promise in
+    ///         return otherMaybePublisher.sinkMaybe(receive: promise)
+    ///     }
     public struct Maybe<Output, Failure: Error>: MaybePublisher {
         public typealias Promise = (MaybeResult<Output, Failure>) -> Void
         typealias Start = (@escaping Promise) -> AnyCancellable
         let start: Start
         
         // TODO: doc
+        // TODO: allow any cancellable
         public init(_ start: @escaping (@escaping Promise) -> AnyCancellable) {
             self.start = start
         }
--- Sources/CombineTraits/TraitPublishers/PublisherTraitsSingle.swift
+++ Sources/CombineTraits/TraitPublishers/PublisherTraitsSingle.swift
@@ -1,6 +1,41 @@
 import Combine
 
 extension TraitPublishers {
+    /// `TraitPublishers.Single` is a ready-made Combine [Publisher] which which
+    /// allows you to dynamically send success or failure events.
+    ///
+    /// It lets you easily create custom single publishers to wrap most
+    /// non-publisher asynchronous work.
+    ///
+    /// You create this publisher by providing a closure. This closure runs when
+    /// the publisher is subscribed to. It returns a cancellable object in which
+    /// you define any cleanup actions to execute when the publisher completes,
+    /// or when the subscription is canceled.
+    ///
+    ///     let publisher = TraitPublishers.Single<String, MyError> { promise in
+    ///         // Eventually send completion event, now or in the future:
+    ///         promise(.success("Alice"))
+    ///         // OR
+    ///         promise(.failure(MyError()))
+    ///
+    ///         return AnyCancellable {
+    ///             // Perform cleanup
+    ///         }
+    ///     }
+    ///
+    /// `TraitPublishers.Single` can be seen as a "deferred future"
+    /// single publisher:
+    ///
+    /// - Nothing happens until the publisher is subscribed to. A new job starts
+    ///   on each subscription.
+    /// - It can complete right on subscription, or at any time in the future.
+    ///
+    /// When needed, `TraitPublishers.Single` can forward its job to another
+    /// single publisher:
+    ///
+    ///     let publisher = TraitPublishers.Single<String, MyError> { promise in
+    ///         return otherSinglePublisher.sinkSingle(receive: promise)
+    ///     }
     public struct Single<Output, Failure: Error>: SinglePublisher {
         public typealias Promise = (Result<Output, Failure>) -> Void
         typealias Start = (@escaping Promise) -> AnyCancellable
