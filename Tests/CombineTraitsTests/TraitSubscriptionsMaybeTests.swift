68c4c67c71cc34dffe0a982a2265385b2e1c17f8
Gwendal Roué <gr@pierlis.com>
2020-11-21 14:31:38 +0100

    More TraitSubscriptions tests

--- Tests/CombineTraitsTests/TraitSubscriptionsMaybeTests.swift
+++ Tests/CombineTraitsTests/TraitSubscriptionsMaybeTests.swift
@@ -35,4 +35,254 @@ class TraitSubscriptionsMaybeTests: XCTestCase {
             }
         }
     }
+    
+    class Witness {
+        var startCalled = false
+        var didCancelCalled = false
+        var didCompleteCalled = false
+    }
+    
+    struct TestError: Error { }
+    
+    func test_completion_empty() throws {
+        struct Publisher: MaybePublisher {
+            typealias Output = Int
+            typealias Failure = TestError
+            
+            let witness: Witness
+            
+            func receive<S>(subscriber: S)
+            where S: Subscriber, Failure == S.Failure, Output == S.Input
+            {
+                let subscription = Subscription(
+                    downstream: subscriber,
+                    context: witness)
+                subscriber.receive(subscription: subscription)
+            }
+            
+            private class Subscription<Downstream: Subscriber>:
+                TraitSubscriptions.Maybe<Downstream, Witness>
+            where
+                Downstream.Input == Output,
+                Downstream.Failure == Failure
+            {
+                override func start(with witness: Witness) {
+                    witness.startCalled = true
+                    receive(.finished)
+                }
+                override func didCancel(with witness: Witness) {
+                    witness.didCancelCalled = true
+                }
+                override func didComplete(with witness: Witness) {
+                    witness.didCompleteCalled = true
+                }
+           }
+        }
+        
+        let witness = Witness()
+        let publisher = Publisher(witness: witness)
+        
+        var value: Int?
+        var completion: Subscribers.Completion<TestError>?
+        _ = publisher.sink(
+            receiveCompletion: {
+                completion = $0
+            },
+            receiveValue: {
+                value = $0
+            })
+
+        XCTAssertNil(value)
+        
+        switch try XCTUnwrap(completion) {
+        case .finished:
+            break
+        case let .failure(error):
+            XCTFail("Unexpected error \(error)")
+        }
+        
+        XCTAssertTrue(witness.startCalled)
+        XCTAssertFalse(witness.didCancelCalled)
+        XCTAssertTrue(witness.didCompleteCalled)
+    }
+    
+    func test_completion_success() throws {
+        struct Publisher: MaybePublisher {
+            typealias Output = Int
+            typealias Failure = TestError
+            
+            let witness: Witness
+            
+            func receive<S>(subscriber: S)
+            where S: Subscriber, Failure == S.Failure, Output == S.Input
+            {
+                let subscription = Subscription(
+                    downstream: subscriber,
+                    context: witness)
+                subscriber.receive(subscription: subscription)
+            }
+            
+            private class Subscription<Downstream: Subscriber>:
+                TraitSubscriptions.Maybe<Downstream, Witness>
+            where
+                Downstream.Input == Output,
+                Downstream.Failure == Failure
+            {
+                override func start(with witness: Witness) {
+                    witness.startCalled = true
+                    receive(.success(1))
+                }
+                override func didCancel(with witness: Witness) {
+                    witness.didCancelCalled = true
+                }
+                override func didComplete(with witness: Witness) {
+                    witness.didCompleteCalled = true
+                }
+           }
+        }
+        
+        let witness = Witness()
+        let publisher = Publisher(witness: witness)
+        
+        var value: Int?
+        var completion: Subscribers.Completion<TestError>?
+        _ = publisher.sink(
+            receiveCompletion: {
+                completion = $0
+            },
+            receiveValue: {
+                value = $0
+            })
+
+        XCTAssertEqual(value, 1)
+        
+        switch try XCTUnwrap(completion) {
+        case .finished:
+            break
+        case let .failure(error):
+            XCTFail("Unexpected error \(error)")
+        }
+        
+        XCTAssertTrue(witness.startCalled)
+        XCTAssertFalse(witness.didCancelCalled)
+        XCTAssertTrue(witness.didCompleteCalled)
+    }
+    
+    func test_completion_failure() throws {
+        struct Publisher: MaybePublisher {
+            typealias Output = Int
+            typealias Failure = TestError
+            
+            let witness: Witness
+            
+            func receive<S>(subscriber: S)
+            where S: Subscriber, Failure == S.Failure, Output == S.Input
+            {
+                let subscription = Subscription(
+                    downstream: subscriber,
+                    context: witness)
+                subscriber.receive(subscription: subscription)
+            }
+            
+            private class Subscription<Downstream: Subscriber>:
+                TraitSubscriptions.Maybe<Downstream, Witness>
+            where
+                Downstream.Input == Output,
+                Downstream.Failure == Failure
+            {
+                override func start(with witness: Witness) {
+                    witness.startCalled = true
+                    receive(.failure(TestError()))
+                }
+                override func didCancel(with witness: Witness) {
+                    witness.didCancelCalled = true
+                }
+                override func didComplete(with witness: Witness) {
+                    witness.didCompleteCalled = true
+                }
+           }
+        }
+        
+        let witness = Witness()
+        let publisher = Publisher(witness: witness)
+        
+        var value: Int?
+        var completion: Subscribers.Completion<TestError>?
+        _ = publisher.sink(
+            receiveCompletion: {
+                completion = $0
+            },
+            receiveValue: {
+                value = $0
+            })
+
+        XCTAssertNil(value)
+        
+        switch try XCTUnwrap(completion) {
+        case .finished:
+            XCTFail("Unexpected success")
+        case let .failure(error):
+            break
+        }
+        
+        XCTAssertTrue(witness.startCalled)
+        XCTAssertFalse(witness.didCancelCalled)
+        XCTAssertTrue(witness.didCompleteCalled)
+    }
+    
+    func test_cancellation() throws {
+        struct Publisher: MaybePublisher {
+            typealias Output = Int
+            typealias Failure = TestError
+            
+            let witness: Witness
+            
+            func receive<S>(subscriber: S)
+            where S: Subscriber, Failure == S.Failure, Output == S.Input
+            {
+                let subscription = Subscription(
+                    downstream: subscriber,
+                    context: witness)
+                subscriber.receive(subscription: subscription)
+            }
+            
+            private class Subscription<Downstream: Subscriber>:
+                TraitSubscriptions.Maybe<Downstream, Witness>
+            where
+                Downstream.Input == Output,
+                Downstream.Failure == Failure
+            {
+                override func start(with witness: Witness) {
+                    witness.startCalled = true
+                }
+                override func didCancel(with witness: Witness) {
+                    witness.didCancelCalled = true
+                }
+                override func didComplete(with witness: Witness) {
+                    witness.didCompleteCalled = true
+                }
+           }
+        }
+        
+        let witness = Witness()
+        let publisher = Publisher(witness: witness)
+        
+        var value: Int?
+        var completion: Subscribers.Completion<TestError>?
+        let cancellable = publisher.sink(
+            receiveCompletion: {
+                completion = $0
+            },
+            receiveValue: {
+                value = $0
+            })
+        cancellable.cancel()
+
+        XCTAssertNil(value)
+        XCTAssertNil(completion)
+        
+        XCTAssertTrue(witness.startCalled)
+        XCTAssertTrue(witness.didCancelCalled)
+        XCTAssertFalse(witness.didCompleteCalled)
+    }
 }
